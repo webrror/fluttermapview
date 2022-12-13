878877eb4a97e5690cc36816e58c0e5d0ff56f7a
Jerin <jerinjacob0206@gmail.com>
2022-12-13 15:05:25 +0530

    added comments

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+// Make sure to store the following in .env file at root of project : API_KEY=your_api_key
+
 void main() {
   runApp(const MyApp());
 }
@@ -17,15 +19,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MapSample(),
@@ -44,16 +37,23 @@ class MapSampleState extends State<MapSample> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
+  // Default Location when app is opened
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+    target:
+        LatLng(37.42796133580664, -122.085749655962), // Location coordinates
+    zoom:
+        14.4746, // Zoom level (0.0 : default, increasing results camera to be closer to earth)
   );
 
-  static const CameraPosition _kApple = CameraPosition(
+  // Jump to this location button press
+  static const CameraPosition _kApplePark = CameraPosition(
       bearing: 0.0, // Direction - 0.0: North facing, 90.0: East facing
-      target: LatLng(37.33479732997545, -122.00903637518307),
-      //tilt: 59.440717697143555,
-      zoom: 16);
+      target: LatLng(
+          37.33479732997545, -122.00903637518307), // Location coordinates
+      tilt: 59.440717697143555, // Tilt degree - 0.0: directly facing earth,
+      zoom:
+          16 // Zoom level (0.0 : default, increasing results camera to be closer to earth)
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -68,16 +68,15 @@ class MapSampleState extends State<MapSample> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('Show Apple Campus'),
-        
-        //icon: const Icon(Icons.directions_boat),
+        onPressed: _goToTheApplePark,
+        label: const Text('Show Apple Park'),
+        icon: const Icon(Icons.navigation_rounded),
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _goToTheApplePark() async {
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kApple));
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kApplePark));
   }
 }
